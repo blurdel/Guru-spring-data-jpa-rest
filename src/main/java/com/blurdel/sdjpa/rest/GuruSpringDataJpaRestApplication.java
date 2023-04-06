a2d5a1e60e2367e337659021a251b6b9e3336293
blurdel <5648737+blurdel@users.noreply.github.com>
2023-04-06 15:32:52 -0400

    Demo project for Spring Data JPA REST - initial commit

---
+++ src/main/java/com/blurdel/sdjpa/rest/GuruSpringDataJpaRestApplication.java
@@ -0,0 +1,13 @@
+package com.blurdel.sdjpa.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruSpringDataJpaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruSpringDataJpaRestApplication.class, args);
+	}
+
+}
