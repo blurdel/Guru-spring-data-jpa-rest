0871116b36fb7fdbfc482810e764157fd287d3e6
blurdel <5648737+blurdel@users.noreply.github.com>
2023-04-06 16:06:52 -0400

    initial setup

---
+++ src/main/java/com/blurdel/sdjpa/rest/domain/BeerStyleEnum.java
@@ -0,0 +1,6 @@
+package com.blurdel.sdjpa.rest.domain;
+
+public enum BeerStyleEnum {
+
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
---
+++ src/main/java/com/blurdel/sdjpa/rest/repositories/BeerRepo.java
@@ -0,0 +1,21 @@
+package com.blurdel.sdjpa.rest.repositories;
+
+import com.blurdel.sdjpa.rest.domain.Beer;
+import com.blurdel.sdjpa.rest.domain.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerRepo extends JpaRepository<Beer, UUID> {
+
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+
+    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+
+    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    Beer findByUpc(String upc);
+
+}
