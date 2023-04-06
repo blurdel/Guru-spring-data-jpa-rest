ad52a2c4b31707421dcb122a95852ec3e48adb86
blurdel <5648737+blurdel@users.noreply.github.com>
2023-04-06 16:50:24 -0400

    Set the REST api base path and URL path

--- src/main/java/com/blurdel/sdjpa/rest/repositories/BeerRepo.java
+++ src/main/java/com/blurdel/sdjpa/rest/repositories/BeerRepo.java
@@ -5,9 +5,11 @@ import com.blurdel.sdjpa.rest.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepo extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
